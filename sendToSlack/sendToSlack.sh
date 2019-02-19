15b94fb2e1c8a26f3a902bbf5b856eba46e019f6
ahugla <ahugla@vmware.com>
2019-02-19 19:10:47 +0100

    initial

---
+++ sendToSlack/sendToSlack.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------
+#      VERSIONING
+# v1.0 - 19 fevrier 2019
+#-----------------------------------------------------------------
+
+#-----------------------------------------------------------------
+#      DESCRIPTION
+# Envoie un message dans le channel shwrfr de slack
+#-----------------------------------------------------------------
+
+#-----------------------------------------------------------------
+#      UTILISATION
+# ./script.sh MESSAGE
+#-----------------------------------------------------------------
+
+
+Message=$1
+CONTENT_FILE="./content_file"
+logFile="sendToSlack.log"
+
+echo "**************** NEW RUN ****************" >> $logFile
+date +"%d-%b-%y  -  %T" >> $logFile
+echo "$Message" >> $logFile
+
+
+# creation du fichier json de contenu
+rm -f $CONTENT_FILE
+echo "{\"text\":\"$Message\"}" >  $CONTENT_FILE
+
+
+# envoi sur slack
+curl -X POST -H 'Content-type: application/json' --data @$CONTENT_FILE  https://hooks.slack.com/services/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
+
+
+rm -f $CONTENT_FILE
